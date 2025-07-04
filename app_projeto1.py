30f981846e724ef6978bc58943111cca874f056f
Sergiogalera125 <sergio_galera@hotmail.com>
2025-07-04 20:43:21 -0300

    Resolvi conflito no app_projeto1.py

--- app_projeto1.py
+++ app_projeto1.py
@@ -1,10 +1,24 @@
 import streamlit as  st
+import pandas as pd
+
 
 st.title("Hello Streamlit")
 st.write("Este é o meu primeiro app com Streamlit!")
-st.header("Este é um 3333")
-st.subheader("Este é um 4444")
-st.text("Este é um 5555")
-st.markdown("Este é um **6666**")
+st.header("Este é um header")
+st.subheader("Este é um subheader4")
+st.text("Este é um texto simples")
+st.markdown("Este é um **6666tesxto em negrito**")
 st.latex(r'''e^{i\pi} + 1 = 0''')
+
+df =pd.DataFrame({
+    'Nome': ['Alice', 'Bob', 'Charlie'],
+    'Idade': [25, 30, 35],
+    'Cidade': ['Nova York', 'Los Angeles', 'Chicago']
+    'Salario': [70000, 80000, 90000]
+})
+
+st.dataframe(df)
+st.table(df)
+
+
 
