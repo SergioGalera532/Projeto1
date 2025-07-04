2f92e831c2cb35c036f5195cb12fa2524d242e23
Sergiogalera125 <sergio_galera@hotmail.com>
2025-07-04 20:29:57 -0300

    Resolvi conflito no app_projeto1.py

--- app_projeto1.py
+++ app_projeto1.py
@@ -3,4 +3,8 @@ import streamlit as  st
 st.title("Hello Streamlit")
 st.write("Este é o meu primeiro app com Streamlit!")
 st.header("Este é um 3333")
+st.subheader("Este é um 4444")
+st.text("Este é um 5555")
+st.markdown("Este é um **6666**")
+st.latex(r'''e^{i\pi} + 1 = 0''')
 
