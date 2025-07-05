979623b91a5a938f23ee5b5cd6eaf973f41e0d1e
Sergiogalera125 <sergio_galera@hotmail.com>
2025-07-04 21:53:05 -0300

    Resolvi conflito no app_projeto1.py

--- app_projeto1.py
+++ app_projeto1.py
@@ -1,5 +1,6 @@
 import streamlit as  st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 st.title("Hello Streamlit")
@@ -7,19 +8,31 @@ st.write("Este é o meu primeiro app com Streamlit!")
 st.header("Este é um header")
 st.subheader("Este é um subheader4")
 st.text("Este é um texto simples")
-st.markdown("Este é um **6666tesxto em negrito**")
+st.markdown("Este é um **tesxto em negrito**")
 st.latex(r'''e^{i\pi} + 1 = 0''')
 
 data = {
-    'Nome': ['Alice', 'Bob', 'Charlie'],
-    'Idade': [25, 30, 35],
-    'Cidade': ['Nova York', 'Los Angeles', 'Chicago'],
-    'Salario': [70000, 80000, 90000]
+    'Nome': ['Ana', 'Bruno', 'Carlos'],
+    'Idade': [23, 35, 45],
+    'Salario': [5000, 7000, 9000]
 }
 
 df = pd.DataFrame(data)
 st.dataframe(df)
 st.table(df)
 
+fig, ax = plt.subplots()
+ax.bar(df['Nome'], df['Salario'])
+st.pyplot(fig)
+
+if st.button('Clique aqui'):
+    st.write('Botão clicado vez!')
+
+idade = st.slider('Selecione sua idade', 0, 100, 25)
+st.write(f'Idade selecionada: {idade}')
+
+opcao = st.selectbox('Escolha um departamento', ['Recursos Humanos', 'TI', 'Vendas'])
+
+st.write(f'Departamento selecionado: {opcao}')
 
 
